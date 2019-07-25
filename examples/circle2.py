228a8f4cba494d079c91ccf049fe2e5880fad258
Ioannis Tziokas <tziokioa@users.noreply.github.com>
2019-07-25 11:57:40 +0200

    Add files via upload

--- examples/circle2.py
+++ examples/circle2.py
@@ -1,11 +1,17 @@
-# -*- coding: utf-8 -*-
-import math
+from pyglet import gl
 
-class Kruh:
-    def obvodObsah(self):
-        print ("Obvod kruhu:", 2 * math.pi * self.polomer)
-        print ("Obsah kruhu:", math.pi * self.polomer**2)
+def draw():
+    window.clear()
 
-kruh1 = Kruh()
-kruh1.polomer=5
-kruh1.obvodObsah()
+    for x_offset in (-window.width, 0, window.width):
+        for y_offset in (-window.height, 0, window.height):
+            # Remember the current state
+            gl.glPushMatrix()
+            # Move everything drawn from now on by (x_offset, y_offset, 0)
+            gl.glTranslatef(x_offset, y_offset, 0)
+
+            # Draw
+            batch.draw()
+
+            # Restore remembered state (this cancels the glTranslatef)
+            gl.glPopMatrix()
